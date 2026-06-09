5eae0ae5fe8789366cf1f65c50e7978c3ef2d930
Shreya Desai <shreyaddesai11@gmail.com>
2026-06-09 17:01:36 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (38.57%) - LeetSync

---
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        
+        if not root:
+            return []
+        
+        result = []
+        stack = [root]
+        
+        while stack:
+            
+            node = stack.pop()
+            result.append(node.val)
+            
+            if node.right:
+                stack.append(node.right)
+            
+            if node.left:
+                stack.append(node.left)
+        
+        return result
