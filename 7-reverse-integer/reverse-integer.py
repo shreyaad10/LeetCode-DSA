e70d601b47a16e3c9358ba348e99ee86a19cd168
Shreya Desai <shreyaddesai11@gmail.com>
2026-06-09 17:12:59 +0530

    Time: 16 ms (70.02%) | Memory: 12.1 MB (99.14%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,21 +1,9 @@
 class Solution(object):
     def reverse(self, x):
-        """
-        :type x: int
-        :rtype: int
-        """
         
         sign = -1 if x < 0 else 1
-        x = abs(x)
         
-        rev = 0
-        
-        while x:
-            digit = x % 10
-            rev = rev * 10 + digit
-            x //= 10
-        
-        rev *= sign
+        rev = int(str(abs(x))[::-1]) * sign
         
         if rev < -2**31 or rev > 2**31 - 1:
             return 0
