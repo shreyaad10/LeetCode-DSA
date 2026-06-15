dbdccd65ee998e36c79420e9940e4682666af8f6
Shreya Desai <shreyaddesai11@gmail.com>
2026-06-15 15:13:31 +0530

    Time: 2 ms (58.03%) | Memory: 12.5 MB (15.16%) - LeetSync

---
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        
+        dummy = ListNode(0)
+        dummy.next = head
+        
+        prev = dummy
+        curr = head
+        
+        while curr:
+            
+            if curr.next and curr.val == curr.next.val:
+                
+                dup = curr.val
+                
+                while curr and curr.val == dup:
+                    curr = curr.next
+                
+                prev.next = curr
+            
+            else:
+                prev = curr
+                curr = curr.next
+        
+        return dummy.next
