48d133df1746906896ce776392d5a2821921bd8c
Shreya Desai <shreyaddesai11@gmail.com>
2026-06-15 15:17:33 +0530

    Time: 67 ms (73.21%) | Memory: 91.6 MB (40.05%) - LeetSync

---
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def deleteMiddle(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        
+        if not head or not head.next:
+            return None
+        
+        slow = head
+        fast = head
+        prev = None
+        
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+        
+        prev.next = slow.next
+        
+        return head
