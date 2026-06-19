c8b56699ecb92174da15497406e72ff8b7c87ea3
Shreya Desai <shreyaddesai11@gmail.com>
2026-06-19 17:56:27 +0530

    Time: 27 ms (34.35%) | Memory: 13.4 MB (43.68%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def singleNumber(self, nums):
+        result = 0
+
+        for i in range(32):
+            bit_count = 0
+
+            for num in nums:
+                if (num >> i) & 1:
+                    bit_count += 1
+
+            if bit_count % 3:
+                result |= (1 << i)
+
+        # Handle negative numbers
+        if result >= 2**31:
+            result -= 2**32
+
+        return result
