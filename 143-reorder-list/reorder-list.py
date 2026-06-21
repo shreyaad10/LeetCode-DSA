2e06d269b754e17922a7d4c14013622a80098ad6
Shreya Desai <shreyaddesai11@gmail.com>
2026-06-21 23:45:53 +0530

    Time: 7 ms (66.98%) | Memory: 30.1 MB (33.47%) - LeetSync

---
+++ 143-reorder-list/reorder-list.py
@@ -0,0 +1,36 @@
+class Solution:
+    def reorderList(self, head):
+        if not head or not head.next:
+            return
+
+        # Find middle
+        slow = fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # Reverse second half
+        prev = None
+        curr = slow.next
+        slow.next = None
+
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+
+        # Merge two halves
+        first = head
+        second = prev
+
+        while second:
+            tmp1 = first.next
+            tmp2 = second.next
+
+            first.next = second
+            second.next = tmp1
+
+            first = tmp1
+            second = tmp2        
