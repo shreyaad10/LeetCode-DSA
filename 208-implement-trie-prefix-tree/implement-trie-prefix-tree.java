2136c90dc4f4f683468adccad006a682d4808611
Shreya Desai <shreyaddesai11@gmail.com>
2026-07-04 14:17:05 +0530

    Time: 34 ms (53.78%) | Memory: 61.8 MB (73.71%) - LeetSync

---
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -0,0 +1,64 @@
+class Trie {
+
+    class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        boolean isEnd;
+    }
+
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+
+        TrieNode curr = root;
+
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+
+            if (curr.children[index] == null) {
+                curr.children[index] = new TrieNode();
+            }
+
+            curr = curr.children[index];
+        }
+
+        curr.isEnd = true;
+    }
+
+    public boolean search(String word) {
+
+        TrieNode curr = root;
+
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+
+            if (curr.children[index] == null) {
+                return false;
+            }
+
+            curr = curr.children[index];
+        }
+
+        return curr.isEnd;
+    }
+
+    public boolean startsWith(String prefix) {
+
+        TrieNode curr = root;
+
+        for (char c : prefix.toCharArray()) {
+            int index = c - 'a';
+
+            if (curr.children[index] == null) {
+                return false;
+            }
+
+            curr = curr.children[index];
+        }
+
+        return true;
+    }
+}
